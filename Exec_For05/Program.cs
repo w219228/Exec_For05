35f996d3eace2d8e9fa493f2452341fa93d3a046
ispan <ispan@DESKTOP-PAKSETB>
2022-10-19 16:40:24 +0800

    complete

---
+++ Exec_For05/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exec_For05
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			for(int i = 1; i <=5; i++)
+			{
+				int add = 5 - i;
+				Console.Write(new string('+',add));
+				for(int k=1;k<=i;k++)
+				{
+					Console.Write(i);
+				}
+				Console.WriteLine();
+			}
+		}
+	}
+}
